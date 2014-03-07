c7ce5ade5b948e89544abbb84e70f5b5455bf2bc
piaoyao <piaoyao424@gmail.com>
2014-03-07 20:43:21 +0800

    20140307
    1.事故处理
    2.联系保险

---
+++ src/com/btten/hcb/accident/AccidentActivity.java
@@ -0,0 +1,70 @@
+package com.btten.hcb.accident;
+
+import com.btten.base.BaseActivity;
+import com.btten.hcb.insurance.InsuranceCallActivity;
+import com.btten.hcb.search.SearchActivity;
+import com.btten.hcbvip.R;
+import com.btten.vincenttools.CallTelephone;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+
+public class AccidentActivity extends BaseActivity {
+
+	private Integer[] linearId = { R.id.accident_camera_linear,
+			R.id.accident_judge_linear, R.id.accident_call_linear,
+			R.id.accident_commission_linear };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.accident_activity);
+		initView();
+	}
+
+	// 初始化控件
+	public void initView() {
+		for (int i = 0; i < linearId.length; i++) {
+			LinearLayout layout = (LinearLayout) findViewById(linearId[i]);
+			layout.setOnClickListener(clickListener);
+		}
+	}
+
+	OnClickListener clickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = null;
+			switch (v.getId()) {
+			case R.id.accident_camera_linear:
+				intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				break;
+			case R.id.accident_judge_linear:
+
+				break;
+			case R.id.accident_call_linear:
+				intent = new Intent(AccidentActivity.this,
+						InsuranceCallActivity.class);
+				break;
+			case R.id.accident_commission_linear:
+				break;
+			default:
+				break;
+			}
+
+			if (intent != null) {
+				startActivity(intent);
+			}
+		}
+	};
+
+	@Override
+	public void initDate() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/com/btten/hcb/insurance/InsuranceCallActivity.java
@@ -0,0 +1,79 @@
+package com.btten.hcb.insurance;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import com.btten.base.BaseActivity;
+import com.btten.hcbvip.R;
+import com.btten.vincenttools.CallTelephone;
+
+public class InsuranceCallActivity extends BaseActivity {
+	private int[] linearID = { R.id.insurance_call_linear_95500,
+			R.id.insurance_call_linear_95505, R.id.insurance_call_linear_95512,
+			R.id.insurance_call_linear_95518, R.id.insurance_call_linear_95556,
+			R.id.insurance_call_linear_95585, R.id.insurance_call_linear_95590 };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.insurance_call_activity);
+		initView();
+	}
+
+	public void initView() {
+		for (int i = 0; i < linearID.length; i++) {
+			LinearLayout layout = (LinearLayout) findViewById(linearID[i]);
+			layout.setOnClickListener(listener);
+		}
+	}
+
+	OnClickListener listener = new View.OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			String phone = null;
+			String name = null;
+			switch (v.getId()) {
+
+			case R.id.insurance_call_linear_95500:
+				phone = "95500";
+				name = "太平洋保险";
+				break;
+			case R.id.insurance_call_linear_95505:
+				phone = "95505";
+				name = "天安保险";
+				break;
+			case R.id.insurance_call_linear_95512:
+				phone = "95512";
+				name = "平安保险";
+				break;
+			case R.id.insurance_call_linear_95518:
+				phone = "95518";
+				name = "人民财产保险";
+				break;
+			case R.id.insurance_call_linear_95556:
+				phone = "95556";
+				name = "华安保险";
+				break;
+			case R.id.insurance_call_linear_95585:
+				phone = "95585";
+				name = "中华联合保险";
+				break;
+			case R.id.insurance_call_linear_95590:
+				phone = "95590";
+				name = "太平洋保险";
+				break;
+			default:
+				break;
+			}
+			new CallTelephone(InsuranceCallActivity.this, phone, name);
+		}
+	};
+
+	@Override
+	public void initDate() {
+		// TODO Auto-generated method stub
+
+	}
+}
